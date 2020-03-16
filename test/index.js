ec8d64a5f665d8ac0dcb2d33316909e37d2ea0e6
Ganesh B <ganeshsurfs@gmail.com>
2020-03-16 13:15:18 +0530

    Adding few changes to file path and adding changes to name for php. TODO: Make pathing give 404 message for wrong path or provide url by function for users.

--- test/index.js
+++ test/index.js
@@ -11,7 +11,7 @@ var app = express();
 app.use('/', php.cgi(path.join(__dirname, '../', '/test/php')));
 
 describe('GET /', function() {
-  it('should respond with /index.php', function(done) {
+  it('should respond with /index.php and valid $_SERVER variable', function(done) {
     request(app)
       .get('/')
       .set('Accept', ['application/json', 'text/html'])
@@ -41,6 +41,59 @@ describe('Get /index.php', function() {
         } else {
           assert.match(res.body.$_SERVER.REMOTE_ADDR, /127.0.0.1|::1/);
           assert.match(res.body.$_SERVER.SCRIPT_FILENAME, /index.php$/);
+          done();
+        }
+      });
+  });
+});
+
+
+describe('Get /test/index.php', function() {
+  it('should return a 404 error for /test/index.php', function(done) {
+    request(app)
+      .get('/test/index.php')
+      .set('Accept', ['application/json', 'text/html'])
+      .expect('Content-Type', /html/)
+      .expect(404)
+      .end(function(err, res) {
+        if (err) {
+          done(err);
+        } else {
+          done();
+        }
+      });
+  });
+});
+
+describe('Get /test', function() {
+  it('should return a 404 for /test', function(done) {
+    request(app)
+      .get('/test')
+      .set('Accept', ['application/json', 'text/html'])
+      .expect('Content-Type', /html/)
+      .expect(404)
+      .end(function(err, res) {
+        if (err) {
+          done(err);
+        } else {
+          done();
+        }
+      });
+  });
+});
+
+
+describe('Get /test/', function() {
+  it('should return a 404 for /test/', function(done) {
+    request(app)
+      .get('/test/')
+      .set('Accept', ['application/json', 'text/html'])
+      .expect('Content-Type', /html/)
+      .expect(404)
+      .end(function(err, res) {
+        if (err) {
+          done(err);
+        } else {
           done();
         }
       });
